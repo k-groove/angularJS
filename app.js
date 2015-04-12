c689b4da6938b380c1bd3f3d4f8a2d626942b75f
Barry Oliver <i31770@gmail.com>
2015-04-12 12:25:44 -0400

    added images, bootstrap.min.css
    <img ng-src="{{}}"/>

--- app.js
+++ app.js
@@ -7,8 +7,17 @@
   });
 
   var gems = [
-    { name: 'Azurite', price: 2.95 },
-    { name: 'Bloodstone', price: 5.95 },
-    { name: 'Zircon', price: 3.95 }
+   { name: 'Azurite',
+     price: 2.95,
+     images:[
+    {full: 'images/azurite.jpg', thumb:'images/azurite-thumb.jpg'}
+    ] 
+   },
+    { name: 'Bloodstone', price: 5.95,images:[
+    {full: 'images/bloodstone.jpg', thumb:'images/bloodstone-thumb.jpg'}
+    ] },
+    { name: 'Zircon', price: 3.95,images:[
+    {full: 'images/zircon.jpg', thumb:'images/zircon-thumb.jpg'}
+    ] }
   ];
 })();
