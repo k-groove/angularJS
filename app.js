d1723026b0caba5fb5dd6b2122b6959ea71cee9f
Barry Oliver <i31770@gmail.com>
2015-04-12 14:03:02 -0400

    added tabs and panel controller

--- app.js
+++ app.js
@@ -5,18 +5,29 @@
   app.controller('StoreController', function(){
     this.products = gems;
   });
+  app.controller("PanelController", function(){
+  this.tab = 1;  
+
+  this.selectTab = function(setTab){
+  	this.tab = setTab;
+  };
+  this.isSelected = function(checkTab){
+  	return this.tab === checkTab;
+  };
+  });
 
   var gems = [
    { name: 'Azurite',
+     description: "shiny stuff azurite",
      price: 2.95,
      images:[
     {full: 'images/azurite.jpg', thumb:'images/azurite-thumb.jpg'}
     ] 
    },
-    { name: 'Bloodstone', price: 5.95,images:[
+    { name: 'Bloodstone', description: "shiny stuff bloodstone", price: 5.95,images:[
     {full: 'images/bloodstone.jpg', thumb:'images/bloodstone-thumb.jpg'}
     ] },
-    { name: 'Zircon', price: 3.95,images:[
+    { name: 'Zircon', description: "shiny stuff zircon", price: 3.95,images:[
     {full: 'images/zircon.jpg', thumb:'images/zircon-thumb.jpg'}
     ] }
   ];
